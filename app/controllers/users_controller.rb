705da275602c0d8ba4f60fc0876064e6f8bcc5b6
FelixAGonzales <FelixAGonzales93@gmail.com>
2024-11-12 22:25:02 -0500

    added profile pic in profile page and header

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,19 @@
 class UsersController < ApplicationController
 
+  def index
+    @users = User.all
+    render :index
+  end
+  
   def show
-    @user = User.find_by(id: params[:id] )
-    render :show
+    # @user = User.find_by(id: params[:id] )
+    # render :show
+    render json: {
+      id: current_user.id,
+      name: current_user.name,
+      email: current_user.email,
+      image_url: current_user.image_url,
+    }
   end
 
 
